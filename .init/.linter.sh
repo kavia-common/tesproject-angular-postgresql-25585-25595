549d43df13fde18d7b6da2505741dacd9af736e1
Kavia AI <production@kavia.ai>
2026-01-06 09:51:37 +0000

    Checkpoint 1 - cg876e851f

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/tesproject-angular-postgresql-25585-25595/backend
+dotnet build --no-restore -v quiet -nologo -consoleloggerparameters:NoSummary /p:TreatWarningsAsErrors=false
+LINT_EXIT_CODE=$?
+if [ $LINT_EXIT_CODE -ne 0 ]; then
+  exit 1
+fi
+
